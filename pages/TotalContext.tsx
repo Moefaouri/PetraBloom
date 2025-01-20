1d1eeacbeae274ce99a8355e23bc0a1686ed808b
Moefaouri <mohammadalfaure190@gmail.com>
2025-01-20 09:21:18 +0300

    fix issue 1

--- pages/TotalContext.tsx
+++ pages/TotalContext.tsx
@@ -1,13 +1,18 @@
 import React, { createContext, useState } from "react";
 
-export const CartContext = createContext();
+interface CartContextType {
+    totalItems: number;
+    setTotalItems: React.Dispatch<React.SetStateAction<number>>;
+}
 
-export const CartProvider = ({ children }) => {
-  const [totalItems, setTotalItems] = useState(0);
+export const CartContext = createContext<CartContextType | undefined>(undefined);
 
-  return (
-    <CartContext.Provider value={{ totalItems, setTotalItems }}>
-      {children}
-    </CartContext.Provider>
-  );
+export const CartProvider = ({ children }: { children: React.ReactNode }) => {
+    const [totalItems, setTotalItems] = useState(0);
+
+    return (
+        <CartContext.Provider value={{ totalItems, setTotalItems }}>
+            {children}
+        </CartContext.Provider>
+    );
 };
