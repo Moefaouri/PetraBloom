c4e58812318c23d261bc15ee87778ab8b905eb1e
Moefaouri <mohammadalfaure190@gmail.com>
2025-01-22 14:51:00 +0300

    fav icon

--- pages/nav.tsx
+++ pages/nav.tsx
@@ -99,7 +99,7 @@ const Nav = () => {
                 onClose={() => setIsCartOpen(false)}
               />
             )}
-                  <div className="position-relative">
+          <div className="position-relative">
           <span className="badge-counter">{favorites.size}</span>
           <svg
             onClick={toggleFavorites}
@@ -107,7 +107,7 @@ const Nav = () => {
             height="28px"
             viewBox="0 0 24 24"
             width="28px"
-            style={{ fill: "white", cursor: "pointer" }}
+            style={{ fill: "white", cursor: "pointer", stroke:"black",strokeWidth:"2"}}
           >
             <path
               d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
